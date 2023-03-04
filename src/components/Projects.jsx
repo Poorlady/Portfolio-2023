c24a9c886b726a1ed4a227572b1b540312ca686f
Alif Sani Ekasmara <saniekasmara@gmail.com>
2023-03-04 21:19:20 +0700

    ADD: favicon && projects link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,59 +4,74 @@ import { FaArrowRight } from 'react-icons/fa'
 const Projects = () => {
   return (
     <section className='section' id='projects'>
-        <div className='works'>
-          <div className='container'>
-            <h2>
-              <span>Featured</span>
-              <span>Projects</span>
-            </h2>
-            <ul>
-              <li>
-                <article className='work__card'>
-                  <div className='work__card__img'>
-                    <img src='/projects/easybank.PNG' />
-                  </div>
-                  <div className='work__card__title'>
-                    <p>Landing Page Mockup</p>
-                    <div>
+      <div className='works'>
+        <div className='container'>
+          <h2>
+            <span>Featured</span>
+            <span>Projects</span>
+          </h2>
+          <ul>
+            <li>
+              <article className='work__card'>
+                <div className='work__card__img'>
+                  <img src='/projects/easybank.PNG' />
+                </div>
+                <div className='work__card__title'>
+                  <p>Landing Page Mockup</p>
+                  <div>
+                    <a
+                      target='_blank'
+                      href='https://poorlady.github.io/fem-Easybank/'
+                    >
                       <FaArrowRight />
-                    </div>
-                    <div className='link_desktop'>View</div>
+                    </a>
                   </div>
-                </article>
-              </li>
-              <li>
-                <article className='work__card'>
-                  <div className='work__card__img'>
-                    <img src='/projects/Todo.png' />
-                  </div>
-                  <div className='work__card__title'>
-                    <p>Drag and Drop Todo App</p>
-                    <div>
+                  <div className='link_desktop'>View</div>
+                </div>
+              </article>
+            </li>
+            <li>
+              <article className='work__card'>
+                <div className='work__card__img'>
+                  <img src='/projects/Todo.png' />
+                </div>
+                <div className='work__card__title'>
+                  <p>Drag and Drop Todo App</p>
+                  <div>
+                    <a
+                      target='_blank'
+                      href='https://poorlady.github.io/ts-learn/'
+                    >
                       <FaArrowRight />
-                    </div>
-                    <div className='link_desktop'>View</div>
+                    </a>
                   </div>
-                </article>
-              </li>
-              <li>
-                <article className='work__card'>
-                  <div className='work__card__img'>
-                    <img src='/projects/snakegame.PNG' />
-                  </div>
-                  <div className='work__card__title'>
-                    <p>Snake Game</p>
-                    <div>
+                  <div className='link_desktop'>View</div>
+                </div>
+              </article>
+            </li>
+            <li>
+              <article className='work__card'>
+                <div className='work__card__img'>
+                  <img src='/projects/snakegame.PNG' />
+                </div>
+                <div className='work__card__title'>
+                  <p>Snake Game</p>
+                  <div>
+                    <a
+                      target='_blank'
+                      href='https://poorlady.github.io/scmb-snake-game/'
+                    >
                       <FaArrowRight />
-                    </div>
-                    <div className='link_desktop'>View</div>
+                    </a>
                   </div>
-                </article>
-              </li>
-            </ul>
-          </div>
+                  <div className='link_desktop'>View</div>
+                </div>
+              </article>
+            </li>
+          </ul>
         </div>
-      </section>
+      </div>
+    </section>
   )
 }
 
